c4e964ef9dac3c22a992e70dff0fec4a1f35471b
Roberto Sanz <rsciriano@gmail.com>
2024-04-14 01:03:12 +0200

    Set min_replicas to 0 on container app

--- deploy/terraform/container_apps.tf
+++ deploy/terraform/container_apps.tf
@@ -91,7 +91,7 @@ module "container_apps" {
                 value                = true
             }]
         }]
-        min_replicas                 = 1
+        min_replicas                 = 0
         max_replicas                 = 1
       }
       secrets = [
